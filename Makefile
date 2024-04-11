e774aa557a99d35078942bf54309716bf636e491
Maks Verver <maks@verver.ch>
2024-04-11 02:54:07 +0200

    Add some release instructions.

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+all:
+	@echo 'This project does need to be compiled. (You can run `make test` or `make dist`, though!)'
+
+dist:
+	./build-dist.sh
+
+test:
+	cat extension/ext-comments.js tests/ext-comments-tests.js | node
+
+.PHONY: all dist tests
---
+++ build-dist.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e -E -o pipefail
+
+version=$(sed -ne 's/^ *"version": "\([0-9.]*\)", *$/\1/p' extension/manifest.json)
+if [ -z "$version" ]; then
+  echo "Failed to get extension version from manifest!" 2>&1
+  exit 1
+fi
+
+output=astral-codex-eleven-${version}.zip
+
+rm -f "${output}"
+
+(cd extension && zip -9 ../"${output}" *.* images/*)
