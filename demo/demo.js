0851661120325bde541a201a187a0cc63211e7f5
Maks Verver <maks@verver.ch>
2024-04-12 00:50:01 +0200

    Add options to demo page.

--- demo/demo.js
+++ demo/demo.js
@@ -4,14 +4,18 @@ const fileInput = document.getElementById('file-input');;
 const rootDiv = document.getElementById('ext-comments');
 let comments = undefined;
 
+const longerDateFormat = new Intl.DateTimeFormat('en-US', {
+  month: 'long', day: 'numeric',
+  hour: '2-digit', minute: '2-digit'});
+
+let replaceCommentOptions = {
+  ...REPLACE_COMMENTS_DEFAULT_OPTIONS,
+  collapseDepth: 3,
+  dateFormatShort: longerDateFormat,
+};
+
 function repopulate() {
-  replaceComments(rootDiv, comments, {
-        ...REPLACE_COMMENTS_DEFAULT_OPTIONS,
-        collapseDepth: 0,
-        dateFormatShort: new Intl.DateTimeFormat('en-US', {
-          month: 'long', day: 'numeric',
-          hour: '2-digit', minute: '2-digit'}),
-      });
+  replaceComments(rootDiv, comments, replaceCommentOptions);
 }
 
 function handleFileChange() {
@@ -26,7 +30,18 @@ function handleFileChange() {
   }
 }
 
-fileInput.addEventListener('change', handleFileChange);
+function setDateFormat(value) {
+  replaceCommentOptions.dateFormatShort =
+      value === 'longer'
+          ? longerDateFormat
+          : REPLACE_COMMENTS_DEFAULT_OPTIONS.dateFormatShort;
+  repopulate();
+}
+
+function setCollapseDepth(value) {
+  replaceCommentOptions.collapseDepth = Number(value);
+  repopulate();
+}
 
 // Trigger once at startup, in case the file input is already populated, which
 // can happen when navigating forward and back after selecting a file.
