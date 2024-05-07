bba3107cdd4e010a33476644546f5a6cdc14582f
Pycea <pycean@gmail.com>
2024-05-06 23:46:49 -0700

    Show comments even if zen mode is on.

--- extension/styles.js
+++ extension/styles.js
@@ -8,12 +8,17 @@ const STYLES = {
     .subscribe-footer,
     .post-header .post-ufi,
     .post-footer,
-    #comments-for-scroll,
     .single-post-section,
     .post div:has(.available-content) > div:not(.available-content),
     .footer .footer-buttons,
     .footer .footer-slogan-blurb {
       display: none !important;
+    }
+
+    .ext-comments {
+      border-top: 1px solid var(--color-detail-themed);
+      margin-top: 20px;
+      padding-top: 20px;
     }
   `,
 };
