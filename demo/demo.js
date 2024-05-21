maksverver/astral-codex-eleven

commit 59847eb666daeb5fb1ebee4e482d5a7c8e26cb19

Refactor option processing logic.
  1. Rename processSelfAndChildren() to applyOptions(): “process” is too vague,
     because it doesn't tell us what is processed.

  2. Pass a list of options to apply directly. This has two advantages:

     1. It's no longer necessary to store the optionApiFuncs in the `options`
        object that is passed to the ExtCommentListComponent.
     2. The `force` parameter becomes unnecessary. Instead,
        getOptionsToProcessInitially() calculates a list of options to be
        applied initially based on whether they are enabled or not.

I also fixed the demo page to work with options.

demo/demo.js
@@ -7,9 +7,7 @@ let comments = undefined;
 function setUpCommentOptions() {
   const optionContainer = document.getElementById('comment-options');
   for (const [key, option] of Object.entries(OPTIONS)) {
-    // only add options that modify comments
-    if (!option.processHeader && !option.processComment) return;
-
+    const optionDiv = document.createElement('div');
     optionShadow[key] = option.default;
     const input = document.createElement('input');
     if (typeof option.default === 'boolean') {
@@ -21,12 +19,13 @@ function setUpCommentOptions() {
     input.addEventListener('change', (event) => {
       optionShadow[key] = event.target.value;
       // slight hack to not run handlers if no comments have been loaded
-      if (commentListRoot) option?.onValueChange(event.target.value);
+      if (commentListRoot) option?.onValueChange?.(event.target.value);
     });
     const label = document.createElement('label');
     label.textContent = key;
     label.htmlFor = `${key}-input`;
-    optionContainer.append(input, label);
+    optionDiv.append(input, label);
+    optionContainer.append(optionDiv);
   }
 }
 
@@ -75,14 +74,7 @@ let replaceCommentOptions = {
 };
 
 function repopulate() {
-  if (comments) {
-    const options = Object.values(OPTIONS);
-    const headerFuncs = options.filter((e) => e.hasOwnProperty('processHeader'));
-    const commentFuncs = options.filter((e) => e.hasOwnProperty('processComment'));
-    const optionApiFuncs = new OptionApiFuncs(headerFuncs, commentFuncs);
-    replaceCommentOptions.optionApiFuncs = optionApiFuncs;
-    replaceComments(rootDiv, comments, replaceCommentOptions);
-  }
+  if (comments) replaceComments(rootDiv, comments);
 }
 
 function handleFileChange() {

demo/demo.css
@@ -60,4 +60,5 @@ a:hover {
 .options th {
   text-align: left;
   padding-right: 16px;
+  vertical-align: top;
 }

extension/astral-codex-eleven.js
@@ -137,13 +137,10 @@ class CommentApi {
 
   const commentApi = new CommentApi(postId);
 
-  const options = Object.values(OPTIONS);
-  const optionApiFuncs = options.filter((e) => e.hasOwnProperty('processComment'));
-
   {
     const start = performance && performance.now();
     replaceComments(rootDiv, comments,
-        {...REPLACE_COMMENTS_DEFAULT_OPTIONS, userId, commentApi, commentOrder, optionApiFuncs});
+        {...REPLACE_COMMENTS_DEFAULT_OPTIONS, userId, commentApi, commentOrder});
     const duration = performance && Math.round(performance.now() - start);
     console.info(LOG_TAG, `DOM updated in ${duration} ms.`);
   }

extension/ext-comments.js
@@ -211,14 +211,10 @@ class ExtCommentListComponent {
     for (const child of this.children) child.reverse();
   }
 
-  // If given, keys is an array of keys to call API functions on. Otherwise, all
-  // keys are processed.
-  processAllChildren(keys) {
-    if (Array.isArray(keys) && keys.length === 0) return;
-
-    for (let child of this.children) {
-      child.processSelfAndChildren(keys);
-    }
+  // Applies the given list of options to all descendant comments.
+  applyOptions(options) {
+    if (options.length === 0) return;
+    for (const child of this.children) child.applyOptions(options);
   }
 }
 
@@ -270,7 +266,7 @@ class ExtCommentComponent {
   //  - options is the object passed to replaceComments().
   //
   constructor(parentElem, comment, parentCommentComponent, options) {
-    const {collapseDepth, dateFormatShort, dateFormatLong, optionApiFuncs} = options;
+    const {collapseDepth, dateFormatShort, dateFormatLong} = options;
 
     // Creates DOM nodes for the given comment text, and appends them to the
     // given parent element. This tries to mirror how Substack seems to process
@@ -470,7 +466,6 @@ class ExtCommentComponent {
     }
 
     this.commentData = comment;
-    this.optionFuncs = optionApiFuncs;
     this.threadDiv   = threadDiv;
     this.headerDiv   = commentHeader;
     this.commentDiv  = commentDiv;
@@ -480,20 +475,6 @@ class ExtCommentComponent {
     this.prevSibling = undefined;
     this.nextSibling = undefined;
     this.childList   = childCommentList;
-
-    this.doOptionApiFunctions();
-  }
-
-  // If given, keys is an array of keys to call API functions on. Otherwise, all
-  // keys are processed. Set force to true to call the functions even if the
-  // option value is falsy.
-  doOptionApiFunctions(keys, force=false) {
-    for (const option of this.optionFuncs) {
-      if (keys && !keys.includes(option.key)) continue;
-      if (optionShadow[option.key] || force) {
-        option.processComment(this);
-      }
-    }
   }
 
   setExpanded(expanded) {
@@ -609,11 +590,11 @@ class ExtCommentComponent {
     this.childList.reverse();
   }
 
-  // If given, keys is an array of keys to call API functions on. Otherwise, all
-  // keys are processed.
-  processSelfAndChildren(keys) {
-    this.doOptionApiFunctions(keys, true);
-    this.childList.processAllChildren(keys);
+  // Applies the given list of options to this comment and all descendants.
+  applyOptions(options) {
+    if (options.length === 0) return;
+    for (const option of options) option.processComment(this);
+    this.childList.applyOptions(options);
   }
 }
 
@@ -775,9 +756,6 @@ const REPLACE_COMMENTS_DEFAULT_OPTIONS = Object.freeze({
   // Set to the numeric id of the currently logged-in user, to enable commenting.
   userId: undefined,
 
-  // Holder for all option API functions
-  optionApiFuncs: [],
-
   // Interface used to created/update/delete comments.
   commentApi: COMMENT_API_UNIMPLEMENTED
 });
@@ -806,6 +784,7 @@ function replaceComments(rootElem, comments, options=REPLACE_COMMENTS_DEFAULT_OP
 
   // Add the top-level comments list.
   commentListRoot = new ExtCommentListComponent(rootElem, comments, undefined, options);
+  commentListRoot.applyOptions(getOptionsToProcessInitially());
 
   if (addCommentLink) {
     enableCommentReply(

extension/options.js
@@ -109,7 +109,7 @@ const hideUsersOption = {
   },
   onValueChange(newValue) {
     this.createCachedSet(newValue);
-    reprocessComments(this.key);
+    reprocessComments(this);
   },
   onStart(currentValue) {
     this.createCachedSet(currentValue);
@@ -121,26 +121,51 @@ const hideUsersOption = {
   }
 };
 
-// All options should be added here.
-const optionArray = [
-  // templateOption,
-  removeNagsOptions,
-  zenModeOption,
-  defaultSortOption,
-  hideUsersOption,
-];
+function getValidOptions() {
+  // All options should be added here.
+  const optionArray = [
+    // templateOption,
+    removeNagsOptions,
+    zenModeOption,
+    defaultSortOption,
+    hideUsersOption,
+  ];
+
+  return Object.fromEntries(optionArray.filter((e) => {
+    const [valid, reason] = isValidOption(e);
+    if (!valid) console.error(LOG_OPTION_TAG, 'Invalid option:', reason, e);
+    return valid;
+  }).map((e) => [e.key, e]));
+}
 
 const LOG_OPTION_TAG = '[Astral Codex Eleven] [Option]';
 const OPTION_KEY = 'acxi-options';
-
-// Reprocess all comments with the given option key.
-function reprocessComments(key) {
-  commentListRoot.processAllChildren([key]);
-}
+const OPTIONS = getValidOptions();
 
 // Stores a local copy of the current option values. It should not be modified
 // directly, instead setOption below should be used.
 let optionShadow = {};
+
+// Returns the list of options that are applied to the initial comment list.
+//
+// This includes all valid options that:
+//
+//  - have a processComment() implementation
+//  - are enabled (i.e., the current value is truthy)
+//
+function getOptionsToProcessInitially() {
+  return Object.entries(OPTIONS)
+      .filter(
+          ([key, option]) =>
+            option.hasOwnProperty('processComment') &&
+            Boolean(optionShadow[key]))
+      .map(([key, option]) => option);
+}
+
+// Reprocess all comments with the given option key.
+function reprocessComments(option) {
+  commentListRoot.applyOptions([option]);
+}
 
 async function loadSavedOptions() {
   const v = await chrome.storage.local.get(OPTION_KEY).catch((e) => {
@@ -234,12 +259,3 @@ function isValidOption(option) {
 
   return [true, undefined];
 }
-
-// OPTIONS maps option keys to option objects.
-const OPTIONS = Object.fromEntries(optionArray.filter((e) => {
-  const [valid, reason] = isValidOption(e);
-  if (!valid) {
-    console.error(LOG_OPTION_TAG, 'Invalid option:', reason, e);
-  }
-  return valid;
-}).map((e) => [e.key, e]));

commit ada5717d54dc9603261e60b274d99d0fcd8edc4c

Fix applying of options (accidentally broken in commit 59847eb).

demo/demo.js
@@ -73,7 +73,7 @@ let replaceCommentOptions = {
 };
 
 function repopulate() {
-  if (comments) replaceComments(rootDiv, comments);
+  if (!comments) replaceComments(rootDiv, comments, replaceCommentOptions);
 }
 
 function handleFileChange() {