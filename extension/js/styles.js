0396fe309cfda50fe1a20ef6264bf0e1c52f462f
Pycea <pycean@gmail.com>
2024-05-22 05:52:45 -0700

    Move option css to separate files.

--- extension/js/styles.js
+++ extension/js/styles.js
@@ -1,47 +1,21 @@
 'use strict';
 
-// Holds dynamically enabled styling for use in options.
-const STYLES = {
-  removeNags: `
-    button[data-testid='noncontributor-cta-button'],
-    .end-of-post-recommend-cta-container,
-    .post-end-cta-full,
-    .subscribe-footer {
-      display: none !important;
-    }
-  `,
-  zenMode: `
-    .end-of-post-recommend-cta-container,
-    .post-end-cta-full,
-    .subscribe-footer,
-    .post-header .post-ufi,
-    .post-footer,
-    .single-post-section,
-    .post div:has(.available-content) > div:not(.available-content),
-    .footer .footer-buttons,
-    .footer .footer-slogan-blurb {
-      display: none !important;
-    }
-
-    .ext-comments {
-      border-top: 1px solid var(--color-detail-themed);
-      margin-top: 20px;
-      padding-top: 20px;
-    }
-  `,
-};
+function optionKeyToCssId(key) {
+  return `${key}-css`;
+}
 
 // Adds a style tag with values from the given key in the STYLES dict.
 function addStyle(key) {
-  const css = STYLES[key];
-  const style = document.createElement('style');
-  style.id = `${key}-css`;
-  style.textContent = css;
-  document.documentElement.appendChild(style);
+  const link = document.createElement('link');
+  link.href = chrome.runtime.getURL(`css/options/${key}.css`);
+  link.id = optionKeyToCssId(key);
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.body.appendChild(link);
 }
 
 // Enables or disables the style created with the given key.
 function setStyleEnabled(key, enabled) {
-  const style = document.getElementById(`${key}-css`);
+  const style = document.getElementById(optionKeyToCssId(key));
   if (style) style.disabled = !enabled;
 }
