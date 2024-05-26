fa77765380b69fa17dd3d3983f20ef9585dfe3be
Maks Verver <maks@verver.ch>
2024-05-26 14:52:56 +0200

    Merge styles.js into options.js.
    styles.js only contained a single, small function that was only used to
    implement options, so it might as well go in options.js with the rest of the
    option API.

--- extension/js/options.js
+++ extension/js/options.js
@@ -335,6 +335,23 @@ const {
     return [true, undefined];
   }
 
+  const STYLE_ELEMS = {};
+
+  function setStyleEnabled(key, enabled) {
+    let link = STYLE_ELEMS[key];
+    if (!link) {
+      // Tiny optimization: don't add <link> element until style is enabled.
+      if (!enabled) return;
+      link = document.createElement('link');
+      link.href = chrome.runtime.getURL(`css/options/${key}.css`);
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      document.body.appendChild(link);
+      STYLE_ELEMS[key] = link;
+    }
+    link.disabled = !enabled;
+  }
+
   // Keep this list in sync with the object at the top of the file.
   return {
     OPTIONS,
